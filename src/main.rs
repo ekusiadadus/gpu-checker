c086a170583c35f9f3a7d99806e9fd2c23f9d6e8
ekusiadadus <ekusiadadus@gmail.com>
2024-03-30 05:40:24 +0900

    chore: update cargo

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use std::process::Command;
 
 #[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
 struct Cli {
-    pattern: String,
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(about = "Show version information")]
+    Version,
+    #[clap(about = "Show GPU memory usage")]
+    Memory,
 }
 
 fn main() -> std::io::Result<()> {
     let args = Cli::parse();
 
-    println!("pattern: {:?}", args.pattern);
-
-    loop {
-        match get_gpu_memory_usage() {
-            Ok(_) => {}
-            Err(e) => eprintln!("Error: {}", e),
+    match args.command {
+        Commands::Version => {
+            println!("nvidia-smi-rs v{}", env!("CARGO_PKG_VERSION"));
+            Ok(())
         }
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        println!("\x1B[2J\x1B[1;1H");
+        Commands::Memory => loop {
+            match get_gpu_memory_usage() {
+                Ok(_) => {}
+                Err(e) => eprintln!("Error: {}", e),
+            }
+            std::thread::sleep(std::time::Duration::from_secs(1));
+            println!("\x1B[2J\x1B[1;1H");
+        },
     }
 }
 
@@ -38,9 +52,9 @@ fn get_gpu_memory_usage() -> std::io::Result<()> {
                 ) {
                     let usage = (used / total) * 100.0;
                     println!(
-                        "GPU Memory Usage: {:.2}% [{}]",
+                        "GPU Memory Usage: {:.2}% {}",
                         usage,
-                        generate_gauge(usage)
+                        generate_colored_gauge(usage)
                     );
                 }
             }
@@ -51,10 +65,27 @@ fn get_gpu_memory_usage() -> std::io::Result<()> {
 
     Ok(())
 }
-fn generate_gauge(usage: f32) -> String {
+
+fn generate_colored_gauge(usage: f32) -> String {
     let gauge_width = 50; // ゲージの幅
     let filled_length = (gauge_width as f32 * usage / 100.0).round() as usize;
-    let filled = "#".repeat(filled_length);
+
+    let (color, reset) = ("\x1B[48;5;", "\x1B[0m"); // ANSI背景色とリセット
+    let color_code = if usage < 50.0 {
+        "22m" // 緑
+    } else if usage < 75.0 {
+        "226m" // 黄色
+    } else {
+        "196m" // 赤
+    };
+
+    let filled = format!(
+        "{}{}{}{}",
+        color,
+        color_code,
+        " ".repeat(filled_length),
+        reset
+    );
     let empty = " ".repeat(gauge_width - filled_length);
     format!("[{}{}]", filled, empty)
 }
